5cd9f2ce8e2718af780207659960e473a22cd7ad
danielbednarz2 <danielbednarz2@gmail.com>
2022-12-04 04:20:56 -0800

    Testing problems 1-4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    arr = args[0];
+    for (let i = 1; i <= args.length - 1; i++) {
+        if (arr.indexOf(args[i]) != -1) {
+            arr.splice(arr.indexOf(args[i]), 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
